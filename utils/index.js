07751195e1c191eae37593051e15fe6c3270cb38
cal20014 <cal20014@byui.edu>
2024-01-11 21:22:12 -0700

    Refactor getName function in utils/index.js

--- utils/index.js
+++ utils/index.js
@@ -1,19 +1,9 @@
-/* ***********************
- * Authors: Grant Call
- * Course: CSE 341
- * Citations for assistance: Github Copilot
- *************************/
-
 const utils = {};
 const Chance = require("chance");
 const chance = new Chance();
 
 utils.getName = () => {
-  let names = [];
-  for (let i = 0; i < 10; i++) {
-    names.push(chance.name());
-  }
-  return JSON.stringify(names);
+  return chance.name();
 };
 
 module.exports = utils;
